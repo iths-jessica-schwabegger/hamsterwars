73f97e637e4b0a540d235819ba45807f84452968
Jessica <jessica.schwabegger@hotmail.com>
2020-05-13 08:56:03 +0200

    Update games.js

--- routes/games.js
+++ routes/games.js
@@ -22,36 +22,42 @@ router.get("/", async (req, res) => {
 
 //	Sparar en match med formatet { timeStamp: Date, 
 //  contestants: [{ hamsterobject }, { hamsterobject } ] }.
+
+
+//dokumentation vid POST: 
+//{
+//	"contestants": [{"id": 7}, {"id": 5}]
+//}
 router.post("/", async (req, res) => {
     try {    
 
-    let hamsters = [];
-    //Hämtar alla hamster-objekt
-    let snapShot = await db.collection("hamsters").get();
+        let hamsters = [];
+        //Hämtar alla hamster-objekt
+        let snapShot = await db.collection("hamsters").get();
 
-    snapShot.forEach(doc => {
-        hamsters.push(doc.data());
-    })
+        snapShot.forEach(doc => {
+            hamsters.push(doc.data());
+        })
 
-    let contestants = []
-    //Loopar igenom hamsterobj-array och jämför ID med id:t som är POSTat i postman
-    for(let obj of hamsters) {
-        for(let hamster of req.body.contestants) {
+        let contestants = []
+        //Loopar igenom hamsterobj-array och jämför ID med id:t som är POSTat i postman
+        for(let obj of hamsters) {
+            for(let hamster of req.body.contestants) {
 
-            if(obj.id === hamster.id) {
-                contestants.push(obj);
+                if(obj.id === hamster.id) {
+                    contestants.push(obj);
+                }
             }
         }
-    }
-    //Lägger in datum och objekt med rätt ID i objekt.
-    let gameObj = {
-        timeStamp: Date(),
-        contestants: contestants
-        //winner: req.body.winner
-    }
-        //Lägger in objekt i firestore.
-        await db.collection("games").doc().set(gameObj)
-        res.send({ msg: "New game created!"});
+        //Lägger in datum och objekt med rätt ID i objekt.
+        let gameObj = {
+            timeStamp: Date(),
+            contestants: contestants
+            //winner: 
+        }
+            //Lägger in objekt i firestore.
+            await db.collection("games").doc().set(gameObj)
+            res.send({ msg: "New game created!"});
     }
     catch(err) {
         console.error(err)
